974fcb7cae55e0b42869b3ffef1edb7a0b47c51b
Rachel Zeng <ruiqi.zzz@gmail.com>
2021-06-01 19:57:01 -0400

    react component

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,18 +1,33 @@
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/ManuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+import React, { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+      return (
+        <div>
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes} />
         </div>
-      </Navbar>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
